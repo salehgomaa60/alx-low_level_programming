220bb933ff13b9f9a05556770dd7bdd1614b2104
salehgomaa60 <salehgomaazzb@gmail.com>
2024-04-12 18:48:59 -0700

    renaming item to array as pointed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t* table;
-	hash_node_t** item;
+	hash_node_t** array;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
+	table = (hash_table_t*)malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	item = malloc(sizeof(hash_node_t *) * size);
-	if (item == NULL)
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		item[i] = NULL;
+	        array[i] = NULL;
 
-	table->item = item;
+	table->array = item;
 	table->size = size;
 
-	return (table);
+	return table;
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -68,7 +68,7 @@ typedef struct hash_node_s
 typedef struct hash_table_s
 {
 	unsigned long int size;
-	hash_node_t **item;
+	hash_node_t **array;
 } hash_table_t;
 
 /* 0-hash_table_create.c */
